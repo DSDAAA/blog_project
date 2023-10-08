4a174375c92941b6cc96121b2a77eaeb238decb8
dunston <13180841861@163.com>
2023-10-08 18:21:55 +0800

    创建全局异常处理类

---
+++ src/main/java/com/dsdaaa/blogproject/utils/GloablExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.dsdaaa.blogproject.utils;
+
+import com.dsdaaa.blogproject.utils.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GloablExceptionHandler {
+    /**
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return Result.fail().message("出错了...");
+    }
+}
+
